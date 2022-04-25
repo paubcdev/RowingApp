35cea56f0274c88cfd46f2a8de652e3a34fbb19d
Pau B.C <blanescolomina.pau@gmail.com>
2022-04-25 15:06:35 +0200

    small changes (added names, etc)

--- constants.py
+++ constants.py
@@ -1,7 +1,13 @@
 MALE_NAMES = ["Liam", "Noah", "Oliver", "Elijah", "William", "James", "Benjamin", "Lucas", "Henry", "Alexander",
-              "Mason", "Michael", "Ethan", "Daniel", "Jacob", "Logan", "Jackson", "Levi", "Sebastian", "Mateo"]
+              "Mason", "Michael", "Ethan", "Daniel", "Jacob", "Logan", "Jackson", "Levi", "Sebastian", "Mateo",
+              "Jack", "Owen", "Theo", "Aiden", "Sam", "Josh", "David", "Wyatt", "Matt", "Luke", "Ash", "Carter",
+              "Julian", "Gray", "Leo", "Jayden", "Gabriel", "Isaac", "Lincoln", "Anthony", "Hudson", "Dylan",
+              "Ezra", "Thomas", "Charles", "Chris", "Jaxon", "Maverick", "Josiah"]
 
 FEMALE_NAMES = ["Olivia", "Emma", "Ava", "Charlotte", "Sophia", "Amelia", "Isabella", "Mia", "Evelyn", "Harper",
-                "Camila", "Gianna", "Abigail", "Luna", "Ella", "Elizabeth", "Sofia", "Emily", "Avery", "Mila"]
+                "Camila", "Gianna", "Abigail", "Luna", "Ella", "Elizabeth", "Sofia", "Emily", "Avery", "Mila",
+                "Scarlett", "Eleanor", "Madison", "Layla", "Penelope", "Aria", "Chloe", "Grace", "Ellie", "Nora",
+                "Hazel", "Zoey", "Riley", "Victoria", "Lily", "Aurora", "Violet", "Nova", "Hannah", "Emilia", "Zoe",
+                "Stella", "Everly", "Isla", "Leah", "Lillian", "Addison", "Willow", "Lucy", "Paisley"]
 
 SURNAMES = list("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
