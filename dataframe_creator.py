3c1f830413e54490f7d877b6dfd54f8ce967602f
Pau B.C <blanescolomina.pau@gmail.com>
2022-04-25 14:48:29 +0200

    refactored and re-compartmentalized the project

--- dataframe_creator.py
+++ dataframe_creator.py
@@ -1,10 +1,13 @@
-from main import gender, age_group, size
 import data_creator
+import input_data
 import pandas as pd
+
+gender, size, age_group = input_data.input_data()
 
 name_list = data_creator.name_generator(gender, size)
 age_list = data_creator.age_generator(age_group, size)
-weight_list = data_creator.weight_generator(size)
-dic = {'name': name_list, 'age': age_list, 'weight': weight_list}
+weight_list = data_creator.weight_generator(gender, size)
+two_thousand_list = data_creator.two_thousand_generator(gender, age_group, size)
 
+dic = {'name': name_list, 'age': age_list, 'weight': weight_list, '2000m time': two_thousand_list}
 df = pd.DataFrame(dic)
--- main.py
+++ main.py
@@ -1,20 +1,7 @@
 # v0.1-5 rower generation process under construction, now generates 4 variables: name+surname, age, weight and
 # a 2000m time, but just in seconds this time
-import data_creator
-import pandas as pd
+import dataframe_creator
 
 if __name__ == "__main__":
-    gender = input("What gender? ([m]ale, [f]emale) ")
-    size = int(input("How many rowers you want to create? "))
-    age_group = input("Which category? ([j]unior, [s]enior, [m]asters) ")
-
-    name_list = data_creator.name_generator(gender, size)
-    age_list = data_creator.age_generator(age_group, size)
-    weight_list = data_creator.weight_generator(gender, size)
-    two_thousand_list = data_creator.two_thousand_generator(gender, age_group, size)
-
-    dic = {'name': name_list, 'age': age_list, 'weight': weight_list, '2000m time': two_thousand_list}
-    df = pd.DataFrame(dic)
-
-    print("DataFrame")
-    print(df)
+    print("DataFrame:")
+    print(dataframe_creator.df)
