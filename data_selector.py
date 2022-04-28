18217dc051fed67979fa827f3459e2ad242dc41f
Pau B.C <blanescolomina.pau@gmail.com>
2022-04-28 11:06:45 +0200

    updated match cases

--- data_selector.py
+++ data_selector.py
@@ -6,15 +6,15 @@ def selector():
     data = input("[f]astest rowers, [s]lowest rowers, [h]eaviest, [l]ightest, [o]ldest, [y]oungest ")
 
     match data:
-        case 'f':
+        case 'f' | 'F' | 'fastest':
             return data_treatment.fastest
-        case 's':
+        case 's' | 'S' | 'slowest':
             return data_treatment.slowest
-        case 'h':
+        case 'h' | 'H' | 'heaviest':
             return data_treatment.heaviest
-        case 'l':
+        case 'l' | 'L' | 'lightest':
             return data_treatment.lightest
-        case 'o':
+        case 'o' | 'O' | 'oldest':
             return data_treatment.oldest
-        case 'y':
+        case 'y' | 'Y' | 'youngest':
             return data_treatment.youngest
