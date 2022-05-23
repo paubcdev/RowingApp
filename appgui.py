34415514d35a7d847b586c500fa210d64582c4d2
Pau Blanes <blanesclomina.pau@gmail.com>
2022-05-23 23:54:53 +0200

    added category combobox

--- appgui.py
+++ appgui.py
@@ -11,6 +11,7 @@ def root_main():
     # Define frames
     number_frame = LabelFrame(root, text="Number of rowers: ", padx=5, pady=5)
     gender_frame = LabelFrame(root, text="Gender of rowers: ", padx=5, pady=5)
+    category_frame = LabelFrame(root, text="Category of rowers: ", padx=5, pady=5)
 
     # Define labels and entries for number
     number_label = Label(number_frame, text="Enter number: ")
@@ -24,6 +25,14 @@ def root_main():
     gender_combo.current(0)
     gender_combo['state'] = 'readonly'
 
+    # Create dropdown menu for category selection
+    cat_list = ['Select one', 'Junior', 'Senior', 'Masters']
+    category = StringVar()
+    category_label = Label(category_frame, text="Select category: ")
+    category_combo = ttk.Combobox(category_frame, textvariable=category, values=cat_list)
+    category_combo.current(0)
+    category_combo['state'] = 'readonly'
+
     # Set the labels and entries
     number_label.grid(row=0, column=0, sticky=NW)
     number_entry.grid(row=0, column=1, sticky=N)
@@ -31,9 +40,13 @@ def root_main():
     gender_label.grid(row=0, column=0, sticky=NW)
     gender_combo.grid(row=1, column=0, sticky=W)
 
+    category_label.grid(row=0, column=0, sticky=NW)
+    category_combo.grid(row=1, column=0, sticky=W)
+
     # Set the frames
     number_frame.grid(row=0, column=0, padx=5, pady=5, sticky=NW)
     gender_frame.grid(row=0, column=1, padx=5, pady=5, sticky=N)
+    category_frame.grid(row=1, column=0, padx=5, pady=5, sticky=NW)
 
     close = Button(root, text="Close", width=10, command=root.quit)
     close.grid(row=3, column=1, sticky=E)
@@ -42,4 +55,5 @@ def root_main():
     root.mainloop()
 
 
-root_main()
+if __name__ == "__main__":
+    root_main()
