b3792366b4bdaf4cc2ac1eeac121d3fd7b98c879
Pau Blanes <blanescolomina.pau@gmail.com>
2022-05-24 01:30:02 +0200

    bug fix

--- data_treatment.py
+++ data_treatment.py
@@ -4,7 +4,7 @@ fastest = df[df['2000m time'] == df['2000m time'].min()]
 slowest = df[df['2000m time'] == df['2000m time'].max()]
 
 heaviest = df[df['weight'] == df['weight'].max()]
-lightest = df[df['weight'] == df['weight'].max()]
+lightest = df[df['weight'] == df['weight'].min()]
 
 oldest = df[df['age'] == df['age'].max()]
-youngest = df[df['age'] == df['age'].max()]
+youngest = df[df['age'] == df['age'].min()]
